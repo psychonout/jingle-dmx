95b8aee55db476da13bd9f4f3a76786587615052
psychonout <32778999+psychonout@users.noreply.github.com>
2025-07-18 11:13:35 +0300

    feat: ready to fire

--- strobe.py
+++ strobe.py
@@ -1,18 +1,13 @@
-import time
-from random import randint
-
-from loguru import logger
-
 from base_dmx import BaseDMX
 
 
 class Strobe(BaseDMX):
-    def __init__(self):
+    def __init__(self, dmx_channel: int = 11) -> None:
         """Initialize Strobe with specific device index"""
-        super().__init__()
+        super().__init__(dmx_channel)
 
     def set_dimmer(self, value: int | None = None) -> None:
-        self._send(1, value)
+        self._send(0, value)
 
     def set_strobe(self, value: int | None = None) -> None:
         """
@@ -26,33 +21,19 @@ class Strobe(BaseDMX):
         192-223 - Random strobe effect
         224-255 - Strobe full on
         """
-        self._send(2, value)
+        self._send(1, value)
 
     def set_warm_white(self, value: int) -> None:
-        self._send(3, value)
+        self._send(2, value)
 
     def set_cold_white(self, value: int) -> None:
-        self._send(4, value)
+        self._send(3, value)
 
     def set_color(self, value: int) -> None:
-        self._send(5, value)
+        self._send(4, value)
 
     def set_macro(self, value: int) -> None:
-        self._send(6, value)
+        self._send(5, value)
 
     def set_macro_speed(self, value: int) -> None:
-        self._send(7, value)
-
-    def reset(self) -> None:
-        """Reset the strobe device to default settings."""
-        self.set_dimmer(0)
-        self.set_strobe(0)
-        self.set_warm_white(0)
-        self.set_cold_white(0)
-        self.set_color(0)
-        self.set_macro(0)
-        self.set_macro_speed(0)
-
-if __name__ == "__main__":
-    with Strobe() as strobe:
-        strobe.reset()
+        self._send(6, value)
